ee2bfde3a207d4d97ec109707d7c57030dde87e1
Darren DCosta <ddcosta@gmail.com>
2019-05-06 11:40:27 -0400

    Done.

--- app/helpers/helpers.rb
+++ app/helpers/helpers.rb
@@ -1,2 +1,17 @@
 class Helpers
+
+  def self.current_user(session)
+    User.find_by(id: session[:user_id])
+  end
+
+  def self.is_logged_in?(session)
+    User.all.each do |user|
+      if user.id == session[:user_id]
+        return true
+      else
+        false
+      end
+    end
+  end
+
 end
