1a3420ea3221e9886f570f9b74f437e1a16b4f20
KFad <kfadigan@yahoo.com>
2019-05-07 08:26:58 -0400

    Logging in

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,4 @@
+require 'pry'
 require_relative '../../config/environment'
 class ApplicationController < Sinatra::Base
   configure do
@@ -6,22 +7,33 @@ class ApplicationController < Sinatra::Base
     set :session_secret, "secret"
   end
 
-  get '/' do
+   get '/' do
     erb :index
   end
 
   post '/login' do
-
+    @user = User.find_by(:username => params[:username])
+    if @user != nil && @user.password == params[:password]
+      session[:user_id] = @user.id
+      redirect to '/account'
+    end
+    erb :error
   end
 
   get '/account' do
-
+    if Helpers.is_logged_in?(session)
+      # binding.pry
+      @user = Helpers.current_user(session)
+      # binding.pry
+      erb :account
+    else
+      erb :error
+    end
   end
+
 
   get '/logout' do
-
+    session.clear
+    redirect '/'
   end
-
-
 end
-
